415201240e36630d1ceab94ba244336d0d383c02
Jianmin <jianminwei1@gmail.com>
2019-05-12 13:15:44 -0400

    configured MongoDB, but not using it for fetchint data yet

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
 
 const fs = require('fs')
 const authors = require("./data/authors")
@@ -10,28 +12,29 @@ const typeDefs = fs.readFileSync('./typeDefs.graphql', 'UTF-8')
 
 const resolvers = require('./resolvers')
 
-const context = { books, authors }
+// 1. Create Asynchronous Function
+async function start() {
+    const app = express()
+    const MONGO_DB = process.env.DB_HOST
+    const client = await MongoClient.connect(
+        MONGO_DB,
+        { useNewUrlParser: true }
+    )
 
-// 2. Call `express()` to create an Express application
-var app = express()
+    const db = client.db()
+    const context = { db, books, authors  }
+    const server = new ApolloServer({ typeDefs, resolvers, context })
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context,
-    engine: process.env.ENGINE_API_KEY ? true : false
-});
+    server.applyMiddleware({ app })
+    app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+    app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+    
+    app.listen({ port: 4000 }, () =>
+        console.log(
+            `GraphQL Server running at http://localhost:4000${server.graphqlPath}`
+        )
+    )
+}
 
-// 3. Call `applyMiddleware()` to allow middleware mounted on the same path
-server.applyMiddleware({ app })
-
-// 4. Create a home route
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
-
-// Add playground route 
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
-
-// 5. Listen on a specific port
-app.listen({ port: 4000 }, () =>
-    console.log(`GraphQL Server running @http://localhost:4000${server.graphqlPath}`)
-)
+// 5. Invoke start when ready to start
+start()
